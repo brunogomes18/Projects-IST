fcc4e6709bf172d004a34d0c8fb445340c718d0e
brunogomes18 <bggomes18@gmail.com>
2020-02-20 11:36:32 +0000

    array

--- basic.c
+++ basic.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int x = 0;
-    for (;x<10;x++){
+    int array[10];
+    for (int x=0;x<10;x++){
+        array[x]=x;
         printf("%d \n",x);
     }
+
+    printf("array pos 5 -> %d", array[5]);
+
+
     return 0;
 }
